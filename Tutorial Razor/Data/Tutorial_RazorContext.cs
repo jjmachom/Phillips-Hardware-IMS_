355c063a8b9c44105a66597cc6422dd328f626ab
jjmachom <jjmacgroup@gmail.com>
2020-04-23 18:50:00 -0500

    first commit

---
+++ Tutorial Razor/Data/Tutorial_RazorContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Tutorial_Razor.Models;
+
+namespace Tutorial_Razor.Data
+{
+    public class Tutorial_RazorContext : DbContext
+    {
+        public Tutorial_RazorContext (DbContextOptions<Tutorial_RazorContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Tutorial_Razor.Models.Customers> Customers { get; set; }
+    }
+}
---
+++ Tutorial Razor/Models/Suppliers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tutorial_Razor.Models
+{
+    public class Suppliers
+    {
+    }
+}
